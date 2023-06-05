3a98593bcd9963e4e2b01761f8003db57eea3bb9
Кирюшин Виталий <kiryuschin.vitalij@yandex.ru>
2023-06-05 15:28:38 +0300

    Add demo example

---
+++ src/main/java/ru/itmo/PreCrime/PreCrimeApplication.java
@@ -0,0 +1,22 @@
+package ru.itmo.PreCrime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class PreCrimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreCrimeApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
+
+}
---
+++ src/test/java/ru/itmo/PreCrime/PreCrimeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmo.PreCrime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreCrimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
