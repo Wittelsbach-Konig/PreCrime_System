c30ff38b89d5d5c6eaccbc0745b7a57c170b5262
Кирюшин Виталий <kiryuschin.vitalij@yandex.ru>
2023-06-18 22:43:55 +0300

    Video element

--- src/main/java/ru/itmo/PreCrime/controller/CardController.java
+++ src/main/java/ru/itmo/PreCrime/controller/CardController.java
@@ -1,6 +1,8 @@
 package ru.itmo.PreCrime.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import ru.itmo.PreCrime.model.CrimeCard;
 // import ru.itmo.PreCrime.model.User;
 import ru.itmo.PreCrime.service.FillingCardService;
 
+@Controller
 public class CardController {
     
     private final FillingCardService cardsService;
@@ -23,13 +26,21 @@ public class CardController {
 
     @GetMapping("/cardfill")
     public String getCardFillPage(@ModelAttribute("crimecard") CrimeCard card) {
-        return "/card_fill";
+        //System.out.printf("\nЗаходим в cardfill\n");
+        return "card_fill";
     }
 
     @PostMapping("/cardfill")
-    public String perfectFilling(@ModelAttribute("crimecard") CrimeCard card) {
+    public String perfectFilling(@Valid @ModelAttribute("crimecard") CrimeCard card, BindingResult bindingResult) {
         //registrationService.register(user);
+        //System.out.printf("\nЗаходим в cardfill\n");
+        if (bindingResult.hasErrors()) {
+            System.out.printf("\nОшибка заполнения!\n");
+            return "card_fill";
+        }
+
         cardsService.saveCard(card);
-        return "/police_office";
+        System.out.printf("\nСохранили!\n");
+        return "police_office";
     }
 }
